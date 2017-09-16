6b9fd372d6a0fa34b14dccc225a2dfffa687ec93
Mason Kriegel <mason.kriegel@honeywell.com>
2017-09-15 23:33:01 -0500

    Iterations 2, 3, and 4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  posttitle      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  belongs_to :user
+  validates_presence_of :posttitle
+  validates_presence_of :body
 end
